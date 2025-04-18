9f44e05e3dc2d20f11348b0cb8d83ea5b545fd87
Argatmj <ikomangarga0612@gmail.com>
2025-04-18 22:17:34 +0200

    Improved changing volume function

--- predict.py
+++ predict.py
@@ -3,17 +3,21 @@ import mediapipe as mp
 import tensorflow as tf
 import numpy as np
 import math 
+from collections import deque
 
 N_FRAMES = 15
 THRESHOLD = 90
 label = {
   0 : "left_swipe",
   1 : "right_swipe",
-  2 : "still"
+  2 : "still",
+  3 : "volume"
 }
 INDEX = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 #INDEX = [0,4,8,12,16,20]
+
+#TODO: improve volume and still dataset 
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -65,12 +69,13 @@ def get_hand_size(hand_landmark, w, h):
 def change_volume(img, hand_landmarks):
   thumb, index, w, h = draw_line(img, hand_landmarks)
   size = get_hand_size(hand_landmarks,w,h)
-  min_d, max_d = 0.2, 1.5
+  min_d, max_d = 0.1, 0.6
   dist = calculate_distance(thumb, index) / size 
-  value = (dist - min_d) / (max_d - min_d)
-  value = max(0.0, min(value,1.0))
+  values = (dist - min_d) / (max_d - min_d)
+  value = max(0.0, min(values,1.0))
   percent = value * 100
   print(round(percent))
+  return percent
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
@@ -78,8 +83,10 @@ with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
   frames = []
+  recent_volumes = deque(maxlen=20)
   percentages = []
   text = "Not Recognized"
+  flag = False
   while cap.isOpened():
     success, image = cap.read()
 
@@ -98,8 +105,16 @@ with mp_hands.Hands(
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         #mp_drawing.draw_landmarks(image,hand_landmarks)
-        change_volume(image,hand_landmarks)
-
+        # change volume 
+        if flag: 
+          percent = change_volume(image,hand_landmarks)
+          recent_volumes.append(round(percent))
+          std = np.std(recent_volumes)
+          if len(recent_volumes) == 20 and std < 1.5:
+            print(f"Volume saved : {recent_volumes[-1]}")
+            flag = False
+            recent_volumes.clear()
+  
     # collect all frames
     if len(frames) < N_FRAMES and results.multi_hand_landmarks:
       coords = collectCoords(results.multi_hand_landmarks)
@@ -130,6 +145,8 @@ with mp_hands.Hands(
     if percentages:
       h = max(percentages)
       index = percentages.index(h)
+      if index == 3:
+        flag = True
       if h >= THRESHOLD:
           text = f"{label[index]}"
       else:
